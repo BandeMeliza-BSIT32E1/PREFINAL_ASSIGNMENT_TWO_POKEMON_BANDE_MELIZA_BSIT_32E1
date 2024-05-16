95002a54b50e5cd47657ae151df9f1a2b4df0469
Bande_Meliza_BSIT32E1 <155070275+BandeMeliza-BSIT32E1@users.noreply.github.com>
2024-05-16 19:10:36 +0800

    update

--- PokemonMVC/PokemonMVC/obj/Debug/net8.0/PokemonMVC.AssemblyInfo.cs
+++ PokemonMVC/PokemonMVC/obj/Debug/net8.0/PokemonMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58dcfe8681737c97f0bd2d1d74235139614408fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
